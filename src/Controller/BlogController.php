c0d913478ae3353ef1696e4d69cc37f23738c780
AdelineDb <adeline.dubosc@hotmail.fr>
2019-05-14 17:22:13 +0200

    récupérer les données stockées dans doctrine

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,35 +2,119 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\ORM\Mapping\OrderBy;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog_index")
+     * @return Response
      */
-    public function index()
+    public function index(): Response
     {
-       /* return new Response(
-            '<html><body>Blog Index</body></html>'
-        ); */
+        /* return new Response(
+             '<html><body>Blog Index</body></html>'
+         ); */
 
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table'
+            );
+        }
+        foreach ($articles as $article) {
+            $article->url = preg_replace('/ /', '-', strtolower($article->getTitle()));
+        }
         return $this->render('blog/index.html.twig', [
-            'owner' => 'Adeline',
-            ]);
+            'owner' => 'Adeline', 'articles' => $articles
+        ]);
     }
 
     /**
-     * @Route("/blog/show/{slug}", requirements={"slug"="[a-z0-9-]+"}, defaults={"slug"="Article Sans Titre"}, name="blog_article")
+     * @param string $slug
+     * @Route("/blog/show/{slug}",
+     *     requirements={"slug"="[a-z0-9-]+"},
+     *     defaults={"slug"= null},
+     *     name="blog_article")
+     * @return Response
      */
-    public function show($slug)
+    public function show(?string $slug): Response
     {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No Slug');
+        }
 
-        $slug = str_replace('-', ' ', $slug);
-        $slug = ucfirst($slug);
+        $slug = str_replace('-', ' ', ucwords($slug));
 
-        return $this->render('blog/show.html.twig', ['slug' =>$slug]);
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with ' . $slug . ' title, found in article\'s table.'
+            );
+        }
+
+        return $this->render('blog/show.html.twig',
+            ['article' => $article,
+                'slug' => $slug]);
+    }
+
+    /**
+     * @param string $categoryName The slugger
+     *
+     * @Route("/blog/category/{categoryName}",
+     *     name="blog_category")
+     * @return Response A response instance
+     */
+    public function showByCategory(?string $categoryName): Response
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => $categoryName]);
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No article with ' . $categoryName . ' category, found.'
+            );
+        }
+
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findBy(['category' => $category], ['id'=>'DESC'], 3);
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table'
+            );
+        }
+        foreach ($articles as $article) {
+            $article->url = preg_replace('/ /', '-', strtolower($article->getTitle()));
+
+            if (!$categoryName) {
+                throw $this
+                    ->createNotFoundException('No category has been set for this article');
+            }
+        }
+
+        return $this->render(
+            'blog/category.html.twig',
+            [
+                'articles' => $articles,
+                'category' => $category,
+            ]);
+
+
     }
 }
